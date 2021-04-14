34f2b034d9b599d8de4fa1d5525b8631538a3cb8
Administrator <1755140651@qq.com>
2021-04-14 18:31:57 +0800

    2021/04/14

--- app/src/main/java/com/example/gameplanedemo/GameActivity.java
+++ app/src/main/java/com/example/gameplanedemo/GameActivity.java
@@ -1,14 +1,16 @@
 package com.example.gameplanedemo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.gameplanedemo.game.GameView;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
-
-public class GameActivity extends Activity {
+public class GameActivity extends AppCompatActivity {
 
     private GameView gameView;
     private boolean mBackKeyPressed = false;//记录是否有首次按键
@@ -17,19 +19,8 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        gameView = (GameView) findViewById(R.id.gameView);
-        //0:combatAircraft
-        //1:explosion
-        //2:yellowBullet
-        //3:blueBullet
-        //4:smallEnemyPlane
-        //5:middleEnemyPlane
-        //6:bigEnemyPlane
-        //7:bombAward
-        //8:bulletAward
-        //9:pause1
-        //10:pause2
-        //11:bomb
+        gameView = findViewById(R.id.gameView);
+
         int[] bitmapIds = {
                 R.drawable.plane,
                 R.drawable.explosion,
